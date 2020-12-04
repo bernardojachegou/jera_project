7018907b280b3bd350e48320a35f052226f8955e
Michel Bernardo <bernardoenglish@outlook.com>
2020-12-04 13:51:35 -0400

    setting routes to create the pages

--- src/services/http.js
+++ src/services/http.js
@@ -1,6 +1,9 @@
 import axios from 'axios';
 
-export const client = axios.create({
-  baseURL: "",
+const client = axios.create({
+  baseURL: "https://api.themoviedb.org/3/discover/movie",
   timeout: 1000,
+  headers: { 'Authorization': 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJkZmMzMmExNWMxODU3YzU4MWMzYTRhNGVlMDdmNmYyZSIsInN1YiI6IjVmY2E0ZWY4YTU3NDNkMDA0MDA0MDAzMCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.GPSLPQTp5akyBbLIKEge6ln8Y8sFFUwghrEgcBqxMlg' }
 });
+
+export default client;
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,22 +1,17 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-  </div>
+  <div>Home</div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-import http from "../services/http";
+import client from "../services/http";
 
 export default {
   name: "Home",
-  components: {
-    HelloWorld,
-  },
   created() {
-    http().then(console.log());
+    client.get("/").then((value) => console.log(value));
   },
 };
 </script>
+
+<style scoped>
+</style>
